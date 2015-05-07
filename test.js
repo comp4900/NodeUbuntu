b2ecf3ed86cbc2536bfcf8a919eeffab612683db
U-Bullkelp\Stoney <rogerharvey888@gmail.com>
2015-05-07 16:11:29 -0700

    test.js port 80

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.end('It is working!\n');
-}).listen(3000);
-console.log('Server running at port 3000');
+}).listen(80);
+console.log('Server running at port 80');
