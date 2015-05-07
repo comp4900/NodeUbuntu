bc2f39e4a6e8acbe536d142af3ea0d5d9aac6c2a
U-Bullkelp\Stoney <rogerharvey888@gmail.com>
2015-05-07 16:19:21 -0700

    test.js 0.0.0.0

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.end('It is working!\n');
-}).listen(80);
+}).listen(80,"0.0.0.0");
 console.log('Server running at port 80');
