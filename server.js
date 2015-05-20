d8523ed51118808ff8ebb6b3e4827f26d43ed1ff
U-Bullkelp\Stoney <rogerharvey888@gmail.com>
2015-05-20 10:22:46 -0700

    omeed changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res){
 app.post('/bracket/html/', function(req, res) {
 
     // overwrite index.html with text from received json
-    fs.writeFile('index.html', req.body.html, function(err) {
+    fs.writeFile(__dirname + '/push/bracket_1_juniors.html', req.body.html, function(err) {
         if(err) return console.log(err);
     });
     res.send(""); // send empty string for response
@@ -38,6 +38,80 @@ app.post('/bracket/update/', function(req, res) {
     io.emit('update', req.body.update);
     res.send("");
 
+});
+// Reads report files in "push" folder, generates html code to display them
+app.get('/reports', function(req, res){
+
+    console.log('start reports ' + __dirname + '/push');
+
+    files = fs.readdirSync( __dirname + '/push'); 
+	var bracket = [];
+	var qualification = [];
+	var situation = [];
+	var b = q = s = reports = 0;
+	for(var i = 0; i < files.length; i++)
+	{ 	
+		if(files[i].indexOf(".html") > -1)
+		{	reports = 1;
+			if(files[i].indexOf("bracket") == 0)
+			{
+				bracket[b++] = files[i];
+			}
+			if(files[i].indexOf("qualification") == 0)
+			{
+				qualification[q++] = files[i];
+			}
+			if(files[i].indexOf("situation") == 0)
+			{
+				situation[s++] = files[i];
+			}
+		}
+	}
+	String.prototype.capitalize = function() {
+    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+	};
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var html = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Reports</title></head><body>';   
+   
+   if(reports != 0)
+   {
+		html += '<h3>Reports available to select for real-time updates</h3><br>\n';
+   }
+   else
+   {
+		html += '<h3>There are no real-time reports available at this time, try again later</h3><br>\n';
+   }
+	if(bracket.length > 0){
+		html += '<h5>Bracket:</h5>'
+		html +='<ul>\n';
+		for (var i=0; i < bracket.length; i++) {		
+			html += '<li><a href="http://localhost/push/' + bracket[i] + '">' 
+			+ (bracket[i].substring(0,bracket[i].indexOf("."))).replace(/[0-9]|_/g,' ').capitalize()+'</a></li>\n';	
+		}
+		html +='</ul>';
+	}
+	if(qualification.length > 0){
+		html += '<h5>Qualification:</h5>'
+		html +='<ul>\n';
+		for (var i=0; i < qualification.length; i++) {		
+			html += '<li><a href="http://localhost/push/' + qualification[i] + '">'
+			+ (qualification[i].substring(0,qualification[i].indexOf("."))).replace(/[0-9]|_/g,' ').capitalize()+'</a></li>\n';	
+		}
+		html +='</ul>';
+	}
+	if(situation.length > 0){
+		html += '<h5>Situation:</h5>'
+		html +='<ul>\n';
+		 for (var i=0; i < situation.length; i++) {		
+			html += '<li><a href="http://localhost/push/' + situation[i] + '">'
+			+ (situation[i].substring(0,situation[i].indexOf("."))).replace(/[0-9]|_/g,' ').capitalize() + '</a></li>\n';	
+		}
+		html +='</ul>';
+	}
+    html += '</body></html>';
+    res.end(html, 'utf-8');
+
+    console.log('end reports');
 });
 
 http.listen(80); //start listening on port 80
